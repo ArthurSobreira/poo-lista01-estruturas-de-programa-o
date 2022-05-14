8e70ea478600feea2ca8558479e27021b04f6883
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-13 22:31:25 -0300

    Creation of the main() function

--- src/Lista01_EstruturasDeProgramação/Exerc04_Fibonacci.py
+++ src/Lista01_EstruturasDeProgramação/Exerc04_Fibonacci.py
@@ -1,5 +1,6 @@
 from Exerc03_BinaryConverter import *
 
+
 def fibonacci(number):
     first_number = second_number = 1
     list_fibo = [first_number, second_number]
@@ -13,3 +14,14 @@ def fibonacci(number):
 
 
 def main():
+    number = input_number('Enter the number to verify if it pertains to the Fibonacci sequence: ')
+    list_fibo = fibonacci(number)
+    if number in list_fibo:
+        print(f'The {number} is in the Fibonacci Sequence.')
+    else:
+        print(f'The {number} is not in the Fibonacci Sequence.')
+
+
+if __name__ == '__main__':
+    main()
+
