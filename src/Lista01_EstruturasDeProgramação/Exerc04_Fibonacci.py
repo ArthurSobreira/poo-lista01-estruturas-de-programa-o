d8eaf9c89d4843b80c30193767dcfe5e965b4fec
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-13 22:15:18 -0300

    File name refactoring

---
+++ src/Lista01_EstruturasDeProgramação/Exerc04_Fibonacci.py
@@ -0,0 +1,15 @@
+from Exerc03_BinaryConverter import *
+
+def fibonacci(number):
+    first_number = second_number = 1
+    list_fibo = [first_number, second_number]
+    while True:
+        current_number = first_number + second_number
+        list_fibo.append(current_number)
+        first_number = second_number
+        second_number = current_number
+        if current_number > number:
+            return list_fibo
+
+
+def main():
