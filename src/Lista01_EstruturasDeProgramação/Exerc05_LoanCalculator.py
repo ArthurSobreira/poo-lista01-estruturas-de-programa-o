a20c5e090c69992c62082dd01dde0870eaec5ef3
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-16 19:21:33 -0300

    Creation of the def main()

--- src/Lista01_EstruturasDeProgramação/Exerc05_LoanCalculator.py
+++ src/Lista01_EstruturasDeProgramação/Exerc05_LoanCalculator.py
@@ -3,6 +3,7 @@ from Exerc03_BinaryConverter import *
 
 def main():
     loan = input_number('Enter the loan value: ')
+    portion = input_number('Enter the value of the payment installment: ')
 
 
 if __name__ == '__main__':
