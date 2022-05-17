fe03e770fe09e543c3cb14e2baef6349014b4fd8
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-16 21:30:29 -0300

    Removal of comments

--- src/Lista01_EstruturasDeProgramação/Exerc06_Palindrome.py
+++ src/Lista01_EstruturasDeProgramação/Exerc06_Palindrome.py
@@ -1,14 +1,11 @@
 def palindrome(word):
-    # Separating the characters of the word in a list.
     word_list = []
     for c in word:
         if c.isalpha():
             word_list.append(c.lower())
 
-    # Creating a reversed list of the word_list.
     reversed_list = word_list[::-1]
 
-    # Checking if the lists are the same.
     if word_list == reversed_list:
         return True
     return False
