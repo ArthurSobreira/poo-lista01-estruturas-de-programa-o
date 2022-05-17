226c5bd247c5429a31e1a4e92809ac5ef39fe389
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-16 21:27:36 -0300

    Addition of a condition to append element in the word_list

--- src/Lista01_EstruturasDeProgramação/Exerc06_Palindrome.py
+++ src/Lista01_EstruturasDeProgramação/Exerc06_Palindrome.py
@@ -2,7 +2,8 @@ def palindrome(word):
     # Separating the characters of the word in a list.
     word_list = []
     for c in word:
-        word_list.append(c.lower())
+        if c.isalpha():
+            word_list.append(c.lower())
 
     # Creating a reversed list of the word_list.
     reversed_list = word_list[::-1]
@@ -12,3 +13,14 @@ def palindrome(word):
         return True
     return False
 
+
+def main():
+    word = str(input('Enter a word or phrase to check if it is a Palindrome: '))
+    if palindrome(word):
+        print('Is a Palindrome.')
+    else:
+        print('Not a Palindrome.')
+
+
+if __name__ == '__main__':
+    main()
