b3d24d666e86d3e1cc40742eb565610146e5de86
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-17 10:55:03 -0300

    Creation of the def root_calculator()

---
+++ src/Lista01_EstruturasDeProgramação/Exerc07_RootCalculator.py
@@ -0,0 +1,18 @@
+from Exerc03_BinaryConverter import *
+from math import *
+
+
+def root_calculator(a, b, c):
+    delt = b ** 2 - (4 * a * c)
+    if delt < 0:  # Without real roots
+        return {'root_1': 0, 'root_2': 0, 'status': 0}
+
+    if delt == 0:  # Two real and equal roots
+        root = - b + sqrt(delt) / 2 * a
+        return {'root_1': root, 'root_2': root, 'status': 1}
+
+    if delt > 0:  # Two real and different roots
+        root_1 = - b + sqrt(delt) / 2 * a
+        root_2 = - b - sqrt(delt) / 2 * a
+        return {'root_1': root_1, 'root_2': root_2, 'status': 2}
+    
