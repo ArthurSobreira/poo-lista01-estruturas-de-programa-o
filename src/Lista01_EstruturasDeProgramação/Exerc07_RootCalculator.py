92fdfc66afd6d0d9c2691a6d04c6fb9e1398d904
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-18 09:22:48 -0300

    Addition order of precedence

--- src/Lista01_EstruturasDeProgramação/Exerc07_RootCalculator.py
+++ src/Lista01_EstruturasDeProgramação/Exerc07_RootCalculator.py
@@ -1,18 +1,16 @@
-from Exerc03_BinaryConverter import *
 from math import *
 
 
 def root_calculator(a, b, c):
-    delt = b ** 2 - (4 * a * c)
+    delt = (b ** 2) - (4 * a * c)
     if delt < 0:  # Without real roots
         return {'root_1': 0, 'root_2': 0, 'status': 0}
 
     if delt == 0:  # Two real and equal roots
-        root = - b + sqrt(delt) / 2 * a
+        root = (- b + sqrt(delt)) / (2 * a)
         return {'root_1': root, 'root_2': root, 'status': 1}
 
     if delt > 0:  # Two real and different roots
-        root_1 = - b + sqrt(delt) / 2 * a
-        root_2 = - b - sqrt(delt) / 2 * a
+        root_1 = (- b + sqrt(delt)) / (2 * a)
+        root_2 = (- b - sqrt(delt)) / (2 * a)
         return {'root_1': root_1, 'root_2': root_2, 'status': 2}
-    
