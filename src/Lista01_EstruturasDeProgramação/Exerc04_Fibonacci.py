1d10cde604e24b3bc6e31bb15c48baf96253ee24
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-18 10:19:27 -0300

    List name refactoring

--- src/Lista01_EstruturasDeProgramação/Exerc04_Fibonacci.py
+++ src/Lista01_EstruturasDeProgramação/Exerc04_Fibonacci.py
@@ -3,14 +3,14 @@ from Exerc03_BinaryConverter import *
 
 def fibonacci(number):
     first_number = second_number = 1
-    list_fibo = [first_number, second_number]
+    fibo_list = [first_number, second_number]
     while True:
         current_number = first_number + second_number
-        list_fibo.append(current_number)
+        fibo_list.append(current_number)
         first_number = second_number
         second_number = current_number
         if current_number > number:
-            return list_fibo
+            return fibo_list
 
 
 def main():
