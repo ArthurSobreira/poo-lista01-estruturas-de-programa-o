776083081eedfce73cfc6b52c30cec95952904ea
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-18 15:39:53 -0300

    Error message change

--- src/Lista01_EstruturasDeProgramação/Exerc10_MonthName.py
+++ src/Lista01_EstruturasDeProgramação/Exerc10_MonthName.py
@@ -2,7 +2,6 @@ def month_name(number):
     month_list = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
                   'november', 'december']
     if (number < 1) or (number > 12):
-        print('Invalid month, Try Again!')
+        print('Invalid Month, Try Again!')
     else:
         return month_list[number - 1].title()
-
