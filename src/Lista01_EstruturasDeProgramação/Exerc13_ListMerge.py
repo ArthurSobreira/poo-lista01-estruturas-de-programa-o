9788561d11c0639651d7f7a20ecffc3083d5be12
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-18 19:26:15 -0300

    Addition of variables to sort the lists

--- src/Lista01_EstruturasDeProgramação/Exerc13_ListMerge.py
+++ src/Lista01_EstruturasDeProgramação/Exerc13_ListMerge.py
@@ -1,12 +1,14 @@
 def list_merge(list_01, list_02):
+    org_list_01 = sorted(list_01)
+    org_list_02 = sorted(list_02)
     merged_list = []
     c = 0
     while True:
-        if (c < len(list_01)) and (c < len(list_02)):
-            if list_01[c] < list_02[c]:
-                merged_list.append(list_01[c])
+        if (c < len(org_list_01)) and (c < len(org_list_02)):
+            if org_list_01[c] < org_list_02[c]:
+                merged_list.append(org_list_01[c])
             else:
-                merged_list.append(list_02[c])
+                merged_list.append(org_list_02[c])
             c += 1
         else:
             return merged_list
