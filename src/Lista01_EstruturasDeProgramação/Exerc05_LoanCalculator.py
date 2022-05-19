a67be5efad00a3f7551238f248bc53cad92e4e9a
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-19 08:51:25 -0300

    Change of the main loop

--- src/Lista01_EstruturasDeProgramação/Exerc05_LoanCalculator.py
+++ src/Lista01_EstruturasDeProgramação/Exerc05_LoanCalculator.py
@@ -11,20 +11,21 @@ def main():
     loan = input_number('Enter the loan value: R$ ', float)
     portion = input_number('Enter the value of the payment installment: R$ ', float)
     fees = input_number('Enter the fee value: % ')
-    time = ceil(loan / portion)
     interest = interest_calculator(loan, fees)
 
-    total_portion = portion
     total_inter = interest
-    for c in range(time):
+    debt = loan - (portion - interest)
+    c = 1
+    while debt > 0:
         print()
-        print(f'================= {c + 1}st Month =================')
+        print(f'================= {c}st Month =================')
         print(f'> Value of interest paid: R${interest:.2f}')
         print(f'> Value applied in the loan payment: R${portion - interest:.2f}')
         print(f'> Accumulated interest value: R${total_inter:.2f}')
-        print(f'> Loan value still to be paid: R${loan - total_portion:.2f}')
+        print(f'> Loan value still to be paid: R${debt:.2f}')
         total_inter += interest
-        total_portion += portion
+        debt -= portion - interest
+        c += 1
 
 
 if __name__ == '__main__':
