983026b9681185eb33c8e867a30ca38eb7d8346e
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-19 11:19:32 -0300

    Definition of the def main()

--- src/Lista01_EstruturasDeProgramação/Exerc09_SizeCalculator.py
+++ src/Lista01_EstruturasDeProgramação/Exerc09_SizeCalculator.py
@@ -1,3 +1,4 @@
+from Exerc03_BinaryConverter import *
 from math import *
 
 
@@ -5,3 +6,14 @@ def size_calculator(d, a):
     dhor = d * cos(a)
     size = sqrt((d ** 2) - (dhor ** 2))
     return size
+
+
+def main():
+    distance = input_number('Enter the Distance value: ')
+    angle = input_number('Enter the Angle value: ')
+    size = size_calculator(distance, angle)
+    print(f'The object size is {size:.2f}')
+
+
+if __name__ == '__main__':
+    main()
