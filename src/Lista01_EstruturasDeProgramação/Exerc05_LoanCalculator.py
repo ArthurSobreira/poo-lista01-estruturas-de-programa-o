86baa51f5d06d0b82e60ba671a0f23ad5df0caa8
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-18 21:34:48 -0300

    Definition of the interest_calculator() function

--- src/Lista01_EstruturasDeProgramação/Exerc05_LoanCalculator.py
+++ src/Lista01_EstruturasDeProgramação/Exerc05_LoanCalculator.py
@@ -1,10 +1,17 @@
 from Exerc03_BinaryConverter import *
+from math import *
+
+
+def interest_calculator(capital, fee, time):
+    inter = capital * (fee / 100) * time
+    return inter
 
 
 def main():
-    loan = input_number('Enter the loan value: ')
-    portion = input_number('Enter the value of the payment installment: ')
+    loan = input_number('Enter the loan value: R$ ', float)
+    portion = input_number('Enter the value of the payment installment: R$ ', float)
+    fees = input_number('Enter the fee value: % ')
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
