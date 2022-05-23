fb93a1bb62caf71f1a9a35761045b41930c6f03f
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-23 09:52:54 -0300

    Removal of the getter and setter methods and change in the final() method

--- src/Lista02_Classes/Exerc01_classStudent.py
+++ src/Lista02_Classes/Exerc01_classStudent.py
@@ -15,33 +15,6 @@ class Student:
     def name(self, new_name):
         self.__name = new_name
 
-    # Getter and Setter __test_1_note
-    @property
-    def test_1_note(self):
-        return self.__test_1_note
-
-    @test_1_note.setter
-    def test_1_note(self, new_note):
-        self.__test_1_note = new_note
-
-    # Getter and Setter __test_2_note
-    @property
-    def test_2_note(self):
-        return self.__test_2_note
-
-    @test_2_note.setter
-    def test_2_note(self, new_note):
-        self.__test_2_note = new_note
-
-    # Getter and Setter __work_note
-    @property
-    def work_note(self):
-        return self.__work_note
-
-    @work_note.setter
-    def work_note(self, new_note):
-        self.__work_note = new_note
-
 
 class StudentsNotes(Student):
     def average(self):
@@ -51,23 +24,13 @@ class StudentsNotes(Student):
         return final_note
 
     def final(self, note):
-        if note >= 6:
-            return 0
+        if note <= 6:
+            print(f'Student {self.name} is not in recovery.')
         final_test = 12 - note
         print(f'Student {self.name} must get a {final_test} on the final test to pass.')
 
 
 class StudentManage:
-    @classmethod
-    def register(cls):
-        cls.apart('Wellcome to the Student Manage', 50)
-        reg = cls.input_value("Enter the student's matriculation number: ", int)
-        name = cls.input_value('Enter the student name: ', str).strip().title()
-        test_sco_1 = cls.input_value('Enter the Test Score 1: ', float)
-        test_sco_2 = cls.input_value('Enter the test Score 2: ', float)
-        work_sco = cls.input_value('Enter the work score: ', float)
-        return StudentsNotes(reg, name, test_sco_1, test_sco_2, work_sco)
-
     @staticmethod
     def apart(msg, size):
         print('=' * size)
@@ -85,7 +48,14 @@ class StudentManage:
             else:
                 return number
 
+    @classmethod
+    def register(cls):
+        cls.apart('Wellcome to the Student Manage', 50)
+        reg = cls.input_value("Enter the student's matriculation number: ", int)
+        name = cls.input_value('Enter the student name: ', str).strip().title()
+        test_sco_1 = cls.input_value('Enter the Test Score 1: ', float)
+        test_sco_2 = cls.input_value('Enter the test Score 2: ', float)
+        work_sco = cls.input_value('Enter the work score: ', float)
+        return StudentsNotes(reg, name, test_sco_1, test_sco_2, work_sco)
 
-if __name__ == '__main__':
-    StudentManage.main()
 
