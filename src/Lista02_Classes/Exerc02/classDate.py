696a7c6f367b79bbd30393eb27162d11cea35454
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-23 20:25:09 -0300

    Refactoration in the month_list

--- src/Lista02_Classes/Exerc02/classDate.py
+++ src/Lista02_Classes/Exerc02/classDate.py
@@ -54,6 +54,6 @@ class Date:
         return -1
 
     def month_by_name(self):
-        month_list = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
-                      'november', 'december']
-        return month_list[self.month - 1].title()  
+        month_list = ['january', 'february', 'march', 'april', 'may', 'june', 'july',
+                      'august', 'september', 'october','november', 'december']
+        return month_list[self.month - 1].title()
