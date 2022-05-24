1590941f8572c42642d0c9866b5fe3a2d2d74be9
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-24 10:07:57 -0300

    Formatted date print and conditions change

--- src/Lista02_Classes/Exerc02/classDate.py
+++ src/Lista02_Classes/Exerc02/classDate.py
@@ -1,3 +1,6 @@
+from datetime import *
+
+
 class Date:
     def __init__(self, day, month, year):
         months = [[2], [4, 6, 9, 11], [1, 3, 5, 7, 8, 10, 12]]
@@ -5,23 +8,27 @@ class Date:
         if year > 0:
             if month in range(1, 13):
                 # Months with 30 days
-                if (day in months[1]) and (day in range(1, 31)):
+                if (month in months[1]) and (1 <= day <= 30):
                     valid_date = True
 
                 # Months with 31 days
-                if (day in months[2]) and (day in range(1, 32)):
+                if (month in months[2]) and (1 <= day <= 31):
                     valid_date = True
 
                 # Month with 28/29 days
-                if day in months[0]:
-                    if (Date.leap_year(year)) and (day in range(1, 30)):
+                if month in months[0]:
+                    if (Date.leap_year(year)) and (1 <= day <= 29):
                         valid_date = True
-                    if (not Date.leap_year(year)) and (day in range(1, 29)):
+                    if (not Date.leap_year(year)) and (1 <= day <= 28):
                         valid_date = True
 
         if valid_date:
+            my_date = date(year, month, day)
+            form_date = datetime.strftime(my_date, "%d/%m/%Y")
+            print(f'Date defined as {form_date}.')
             self.__day, self.__month, self.__year = day, month, year
         else:
+            print('Invalid Date, it will be set to 01/01/0001.')
             self.__day, self.__month, self.__year = 1, 1, 1
 
     @property
