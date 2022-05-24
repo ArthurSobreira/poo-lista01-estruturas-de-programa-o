897b1e3cc301a53191b18ac51fee05206ef8c83c
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-24 11:21:38 -0300

    Definition of the def main() and change in the date_input() function

--- src/Lista02_Classes/Exerc02/DateManage.py
+++ src/Lista02_Classes/Exerc02/DateManage.py
@@ -9,9 +9,56 @@ def apart(msg, size):
 
 
 def date_input():
-    apart('Welcome to Date Manager', 50)
     day = input_number('> Enter the Day: ')
     mon = input_number('> Enter the Month (Number): ')
     year = input_number('> Enter the Year: ')
     return DateAttributes(day, mon, year)
 
+
+def main():
+    apart('Welcome to Date Manager', 50)
+    main_date = date_input()
+    while True:
+        print('=' * 50)
+        choice = input_number(
+            "1 - To compare two dates.\n"
+            "2 - To see the month name.\n"
+            "3 - To check if the year is a leap year.\n"
+            "4 - To clone the date.\n"
+            "5 - To go out\n"
+            "> ")
+        if choice == 1:
+            apart('Enter the Date to Compare it', 50)
+            new_date = date_input()
+            comp = main_date.compare(new_date)
+            if comp == 0:
+                print('\033[32mSame dates.\033[m')
+            if comp == 1:
+                print('\033[32mThe First date is bigger.\033[m')
+            else:
+                print('\033[32mThe Second date is bigger.\033[m')
+            continue
+        if choice == 2:
+            mon_name = main_date.month_by_name()
+            print(f"> The name of the month is {mon_name}.")
+            continue
+        if choice == 3:
+            current_year = main_date.year
+            if main_date.leap_year(current_year):
+                print(f'> {current_year} is a leap year.')
+            else:
+                print(f'> {current_year} is not a leap year.')
+            continue
+        if choice == 4:
+            date_clone = main_date.clone()
+            print(f'Object {date_clone} has just been cloned. ')
+            continue
+        if choice == 5:
+            apart('End of the Program, always come back!', 50)
+            break
+        else:
+            print('\033[31mInvalid Value, Try Again!\033[m')
+
+
+if __name__ == '__main__':
+    main()
