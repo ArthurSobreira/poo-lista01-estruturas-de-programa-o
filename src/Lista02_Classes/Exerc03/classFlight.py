4d23de38fbfd93130c32b8e1331e7054deac072a
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-27 07:46:09 -0300

    Add the seat parameter and its getters and setters

--- src/Lista02_Classes/Exerc03/classFlight.py
+++ src/Lista02_Classes/Exerc03/classFlight.py
@@ -2,9 +2,10 @@ from src.Lista02_Classes.Exerc02.classDate import Date
 
 
 class Flight:
-    def __init__(self, number, data):
+    def __init__(self, number, data, seat):
         self.__number = number
         self.__data = Date(data['day'], data['month'], data['year'])
+        self.__seat = seat
 
     # Getter and Setter __number
     @property
@@ -14,6 +15,15 @@ class Flight:
     @number.setter
     def number(self, new_number):
         self.__number = new_number
+
+    # Getter and Setter __seat
+    @property
+    def seat(self):
+        return self.__seat
+
+    @seat.setter
+    def seat(self, new_seat):
+        self.__seat = new_seat
 
     # Getter __data
     @property
