884ea48f2f98d205950ef9eae5a212089bb02b86
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-30 11:10:33 -0300

    Update the classFlightManager.py file

--- src/Lista02_Classes/Exerc03/classFlightManager.py
+++ src/Lista02_Classes/Exerc03/classFlightManager.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from string import ascii_uppercase
 from classFlight import Flight
 
 
@@ -21,21 +20,21 @@ class FlightManager(Flight):
     def vacant_seat(self):  # proximoLivre
         pass
 
-    def check_occupancy(self, seat):  # verifica
+    def check_occupancy(self, seat):
         occupation = False
-        column, row = str(seat[0].upper()), int(seat[1:])
+        column, row = str(seat[0]), int(seat[1:])
         if self.seats_list[column][row] == 'X':
             occupation = True
         return occupation
 
-    def occupy_seat(self, seat):  # ocupa
-        column, row = str(seat[0].upper()), int(seat[1:])
+    def occupy_seat(self, seat):
+        column, row = str(seat[0]), int(seat[1:])
         if not self.check_occupancy(seat):
             self.seats_list[column][row] = 'X'
             return True
         return False
 
-    def number_vacancies(self):  # vagas
+    def number_vacancies(self):
         amount = 0
         df = pd.DataFrame(self.seats_list)
         for c in self.seats_list:
@@ -46,14 +45,3 @@ class FlightManager(Flight):
     def seat_map(self):
         return pd.DataFrame(self.seats_list)
 
-
-if __name__ == '__main__':
-    def seats_list():
-        letters = list(ascii_uppercase[:6])
-        seats = dict()
-        for c in letters:
-            seats[c] = list('.' * 16)
-        return seats
-
-    my_list = seats_list()
-    my_obj = FlightManager(23123, {'day': 2, 'month': 4, 'year': 2076}, my_list)
