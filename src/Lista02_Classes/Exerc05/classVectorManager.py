5339bba38459ecfc819c7c72971df1296c0777ec
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-31 16:00:35 -0300

    Refactor class VectorManage

--- src/Lista02_Classes/Exerc05/classVectorManager.py
+++ src/Lista02_Classes/Exerc05/classVectorManager.py
@@ -3,12 +3,12 @@ from classVector import Vector
 
 class VectorManage(Vector):
     def insert(self, element):
-        self.initial_list.append(element)
+        self.initial_vector.append(element)
 
     def get_element(self, index):
         if index not in range(self.list_size()):
             return False
-        return self.initial_list[index]
+        return self.initial_vector[index]
 
     def list_size(self):
-        return len(self.initial_list)
+        return len(self.initial_vector)
