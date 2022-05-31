ed4d50be942db45c54d32ce4d9a16d2b034ec234
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-31 15:42:47 -0300

    Add __str__() method

--- src/Lista02_Classes/Exerc05/classVector.py
+++ src/Lista02_Classes/Exerc05/classVector.py
@@ -2,6 +2,9 @@ class Vector:
     def __init__(self, initial_list):
         self.__initial_list = initial_list
 
+    def __str__(self):
+        return str(self.initial_list)
+
     # Getter and Setter __initial_list
     @property
     def initial_list(self):
