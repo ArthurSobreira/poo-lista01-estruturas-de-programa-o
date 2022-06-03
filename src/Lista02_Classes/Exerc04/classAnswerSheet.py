30dc690d892244fa3a57f4a92c832979b465af27
Arthur Sobreira <tutusobreira@gmail.com>
2022-06-03 20:42:42 -0300

    Update classAnswerSheet.py file

--- src/Lista02_Classes/Exerc04/classAnswerSheet.py
+++ src/Lista02_Classes/Exerc04/classAnswerSheet.py
@@ -4,7 +4,7 @@ from string import ascii_lowercase
 class AnswerSheet:
     def __init__(self, answers_list):
         answers_dic = {}
-        if len(answers_list) == 3:
+        if len(answers_list) == 15:
             for question, answer in enumerate(answers_list):
                 if answer.lower() in ascii_lowercase[:5]:
                     answers_dic[question + 1] = answer
