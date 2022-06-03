ef0cb3ee1802b8275829bf0294489f6f8e98cda9
Arthur Sobreira <tutusobreira@gmail.com>
2022-06-03 18:42:00 -0300

    Define right_answer() method

--- src/Lista02_Classes/Exerc04/classAnswerSheet.py
+++ src/Lista02_Classes/Exerc04/classAnswerSheet.py
@@ -1,7 +1,7 @@
 class AnswerSheet:
     def __init__(self, answers_list):
         answers_dic = {}
-        if len(answers_list) == 15:
+        if len(answers_list) == 3:
             for question, answer in enumerate(answers_list):
                 if answer.lower() in 'abcde':
                     answers_dic[question + 1] = answer
@@ -17,7 +17,6 @@ class AnswerSheet:
     def answer_sheet(self, new_answer_sheet):
         self.__answer_sheet = new_answer_sheet
 
-
-if __name__ == '__main__':
-    gaba = AnswerSheet(['a', 'c', 'd', 'b', 'e', 'b', 'c', 'a', 'c', 'd', 'b', 'e', 'b', 'c', 'd'])
-    print(gaba.answer_sheet)
+    # Instance Method
+    def right_answer(self, question):
+        return self.answer_sheet[question]
