e98161d067edfdbdc00d385ceb5c611336313fee
Arthur Sobreira <tutusobreira@gmail.com>
2022-06-03 18:13:53 -0300

    Define getter and setter method

--- src/Lista02_Classes/Exerc04/classAnswerSheet.py
+++ src/Lista02_Classes/Exerc04/classAnswerSheet.py
@@ -6,9 +6,18 @@ class AnswerSheet:
                 if answer.lower() in 'abcde':
                     answers_dic[question + 1] = answer
 
-        self.answer_sheet = answers_dic
+        self.__answer_sheet = answers_dic
+
+    # Getter and Setter __answer_sheet
+    @property
+    def answer_sheet(self):
+        return self.__answer_sheet
+
+    @answer_sheet.setter
+    def answer_sheet(self, new_answer_sheet):
+        self.__answer_sheet = new_answer_sheet
 
 
 if __name__ == '__main__':
     gaba = AnswerSheet(['a', 'c', 'd', 'b', 'e', 'b', 'c', 'a', 'c', 'd', 'b', 'e', 'b', 'c', 'd'])
-    print(gaba.answer_sheet[1])
+    print(gaba.answer_sheet)
