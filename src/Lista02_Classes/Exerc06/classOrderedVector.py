06bbd95f04e36319c62bbfbe560b9b67bc98d572
Arthur Sobreira <tutusobreira@gmail.com>
2022-06-03 08:16:48 -0300

    Update classOrderedVector.py file

--- src/Lista02_Classes/Exerc06/classOrderedVector.py
+++ src/Lista02_Classes/Exerc06/classOrderedVector.py
@@ -2,6 +2,9 @@ from src.Lista02_Classes.Exerc05.classVector import Vector
 
 
 class OrderedVector(Vector):
+    def order(self):
+        return sorted(self.initial_vector)
+
     def insert_string(self, string):
         self.initial_vector.append(string)
         self.initial_vector = sorted(self.initial_vector)
