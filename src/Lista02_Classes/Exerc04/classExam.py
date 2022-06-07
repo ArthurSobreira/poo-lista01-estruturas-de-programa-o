38a6ccbf2d94a0c0d7ead40c02c4d3fca2161ac2
Arthur Sobreira <tutusobreira@gmail.com>
2022-06-07 09:03:18 -0300

    Update classExam.py file

--- src/Lista02_Classes/Exerc04/classExam.py
+++ src/Lista02_Classes/Exerc04/classExam.py
@@ -1,2 +1,14 @@
-class Exam:
+from src.Lista02_Classes.Exerc04.classAnswerSheet import AnswerSheet
 
+
+class Exam:
+    def __init__(self, ans_list):
+        self.__answer_sheet = AnswerSheet(ans_list)
+
+    @property
+    def answer_sheet(self):
+        return self.__answer_sheet
+
+    @answer_sheet.setter
+    def answer_sheet(self, new_answer_sheet):
+        self.__answer_sheet = new_answer_sheet
