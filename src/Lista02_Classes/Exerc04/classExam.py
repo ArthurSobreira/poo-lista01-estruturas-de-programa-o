a679af51bb0565c673d637381e40dd426f235036
Arthur Sobreira <tutusobreira@gmail.com>
2022-06-08 17:49:01 -0300

    Update classExam.py file

--- src/Lista02_Classes/Exerc04/classExam.py
+++ src/Lista02_Classes/Exerc04/classExam.py
@@ -3,14 +3,14 @@ from src.Lista02_Classes.Exerc04.classAnswerSheet import AnswerSheet
 
 class Exam:
     def __init__(self, stu_ans_list):
-        self.__student_answers = AnswerSheet(stu_ans_list)
+        self.__obj = AnswerSheet(stu_ans_list)
         self.print_stu_ans_list = AnswerSheet(stu_ans_list).__str__()
 
     # Getter and Setter __student_answers
     @property
     def student_answers(self):
-        return self.__student_answers.answer_sheet
+        return self.__obj.answer_sheet
 
     @student_answers.setter
     def student_answers(self, new_student_answers):
-        self.__student_answers.answer_sheet = new_student_answers
+        self.__obj.answer_sheet = new_student_answers
