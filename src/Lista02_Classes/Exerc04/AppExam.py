62a00e8a857cfd13c2603bccb44fb9a6beef2d51
Arthur Sobreira <tutusobreira@gmail.com>
2022-06-09 10:35:07 -0300

    Update AppExam.py file

--- src/Lista02_Classes/Exerc04/AppExam.py
+++ src/Lista02_Classes/Exerc04/AppExam.py
@@ -34,9 +34,9 @@ def main():
         select = input_number(
             "1 - Answer for a specific question;\n"
             "2 - Number of correct answers;\n"
-            "3 - ;\n"
-            "4 - ;\n"
-            "5 - ;\n"
+            "3 - Exam grade;\n"
+            "4 - Print answers sheet;\n"
+            "5 - Print student answers sheet;\n"
             "6 - Exit.\n"
             "> ")
 
@@ -51,7 +51,28 @@ def main():
 
         if select == 2:
             correct_ans = main_exam.number_right_answers()
-            print(f'\033[32mCorrect Answers: {correct_ans}/15\033[ma')
+            print(f'\033[32mCorrect Answers: {correct_ans}/15\033[m')
+
+        if select == 3:
+            grd = main_exam.exam_grade()
+            print(f'\033[32mExam Grade: {grd}/10\033[m')
+
+        if select == 4:
+            print('=' * 50)
+            form_ans_sht = main_exam.print_ans_list
+            print(form_ans_sht)
+
+        if select == 5:
+            print('=' * 50)
+            form_ans_sht = main_exam.print_stu_ans_list
+            print(form_ans_sht)
+
+        if select == 6:
+            apart('End of the Program, always come back!', 50)
+            break
+
+        if (select < 1) or (select > 6):
+            print('\033[31mInvalid Value, Try Again!\033[m')
 
 
 if __name__ == '__main__':
