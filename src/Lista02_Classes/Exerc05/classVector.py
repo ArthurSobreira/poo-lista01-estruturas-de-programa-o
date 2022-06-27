fa1215633df4e284ad2ce8b47426b69dc2ceae14
Arthur Sobreira <tutusobreira@gmail.com>
2022-06-27 17:09:23 -0300

    Add dunder methods

--- src/Lista02_Classes/Exerc05/classVector.py
+++ src/Lista02_Classes/Exerc05/classVector.py
@@ -5,6 +5,18 @@ class Vector:
     def __str__(self):
         return str(self.initial_vector)
 
+    def __len__(self):
+        return len(self.initial_vector)
+
+    def __getitem__(self, index):
+        try:
+            return self.initial_vector[index]
+        except IndexError:
+            return False
+
+    def insert(self, value):
+        self.initial_vector.append(value)
+
     # Getter and Setter __initial_vector
     @property
     def initial_vector(self):
