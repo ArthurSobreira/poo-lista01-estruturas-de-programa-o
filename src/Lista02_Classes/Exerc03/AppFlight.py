77dbff7d977391a737cfc43e838be1b49d114295
Arthur Sobreira <tutusobreira@gmail.com>
2022-06-27 16:30:58 -0300

    Update AppFlight.py file

--- src/Lista02_Classes/Exerc03/AppFlight.py
+++ src/Lista02_Classes/Exerc03/AppFlight.py
@@ -1,18 +1,7 @@
 from src.Lista01_EstruturasDeProgramacao.Exerc03_BinaryConverter import input_number
 from src.Lista02_Classes.Exerc02.AppDate import apart
 from string import ascii_uppercase
-from classFlightManager import FlightManager
-
-
-def seats_list(lett_list=False):
-    letters = list(ascii_uppercase[:6])
-    if lett_list:
-        return letters
-
-    seats = dict()
-    for c in letters:
-        seats[c] = list('.' * 16)
-    return seats
+from src.Lista02_Classes.Exerc03.classFlight import Flight
 
 
 def seat_input(msg):
@@ -20,7 +9,8 @@ def seat_input(msg):
         seat = str(input(msg)).strip().upper()
         try:
             if (seat[0].isalpha()) and (seat[1:].isalnum()):
-                if (str(seat[0]) in seats_list(True)) and (int(seat[1:]) in range(0, 16)):
+                valid_letters = list(ascii_uppercase[:6])
+                if (str(seat[0]) in valid_letters) and (int(seat[1:]) in range(0, 16)):
                     return seat
             print('\033[31mInvalid seat value, Try Again!\033[m')
             continue
@@ -36,7 +26,7 @@ def flight_data_entry():
     month = input_number('> Month (number) ')
     year = input_number('> Year ')
     main_date = {'day': day, 'month': month, 'year': year}
-    flight_data = FlightManager(num, main_date, seats_list())
+    flight_data = Flight(num, main_date)
     return flight_data
 
 
@@ -95,7 +85,6 @@ def main():
 
         if (choice < 1) or (choice > 6):
             print('\033[31mInvalid Value, Try Again!\033[m')
-            print('=' * 50)
 
 
 if __name__ == '__main__':
