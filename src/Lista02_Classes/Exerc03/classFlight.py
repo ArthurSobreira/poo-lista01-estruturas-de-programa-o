b9ec85d035eeb4c1b40400515df6121e12900fbb
Arthur Sobreira <tutusobreira@gmail.com>
2022-06-27 16:29:19 -0300

    Realocate methods to Flight class

--- src/Lista02_Classes/Exerc03/classFlight.py
+++ src/Lista02_Classes/Exerc03/classFlight.py
@@ -1,8 +1,16 @@
 from src.Lista02_Classes.Exerc02.classDate import Date
+from string import ascii_uppercase
+import pandas as pd
 
 
 class Flight:
     def __init__(self, number, date):
+        letters = list(ascii_uppercase[:6])
+        seats = dict()
+        for c in letters:
+            seats[c] = list('.' * 16)
+
+        self.seats_list = seats
         self.__number = number
         self.__date = Date(date['day'], date['month'], date['year'])
 
@@ -23,3 +31,51 @@ class Flight:
     @property
     def date(self):
         return self.__date
+
+    # Main Methods
+    def next_vacant_seat(self, seat):
+        column, row = str(seat[0]), int(seat[1:])
+        closest_value = []
+        for num, c in enumerate(self.seats_list[column]):
+            if c == '.':
+                if num < row:
+                    diff = row - num
+                    closest_value.append(diff)
+                if num > row:
+                    diff = num - row
+                    closest_value.append(diff)
+
+        if len(closest_value) == 0:  # There are no vacant seats
+            return False
+
+        closest_value = sorted(closest_value, key=int)
+        lower = closest_value[0]
+        closest_seat = row + lower
+        if (closest_seat > 15) or (self.check_occupancy(f'{column}{closest_seat}')):
+            closest_seat = row - lower
+        return f'{column}{closest_seat}'
+
+    def check_occupancy(self, seat):
+        occupation = False
+        column, row = str(seat[0]), int(seat[1:])
+        if self.seats_list[column][row] == 'X':
+            occupation = True
+        return occupation
+
+    def occupy_seat(self, seat):
+        column, row = str(seat[0]), int(seat[1:])
+        if not self.check_occupancy(seat):
+            self.seats_list[column][row] = 'X'
+            return True
+        return False
+
+    def number_vacancies(self):
+        amount = 0
+        df = self.seat_map()
+        for c in self.seats_list:
+            freq = df.groupby([c]).size()
+            amount += freq.values[0]
+        return amount
+
+    def seat_map(self):
+        return pd.DataFrame(self.seats_list)
