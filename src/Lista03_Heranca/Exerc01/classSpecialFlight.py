a4948e363cfb8d793704ec075a874878f7fcc99d
Arthur Sobreira <tutusobreira@gmail.com>
2022-06-28 16:11:07 -0300

    Define seat_type method

--- src/Lista03_Heranca/Exerc01/classSpecialFlight.py
+++ src/Lista03_Heranca/Exerc01/classSpecialFlight.py
@@ -10,7 +10,7 @@ class SpecialFlight(Flight):
         for col in ascii_uppercase[:6]:
             for seat in range(1, row_smoking_seats+1):
                 self.seats_list[col][-seat] = 's'
-        self.__row_smoking_seats = (row_smoking_seats * 6)
+        self.__row_smoking_seats = row_smoking_seats
 
     # Getter and Setter __row_smoking_seats
     @property
@@ -37,7 +37,17 @@ class SpecialFlight(Flight):
                 if seat == 's':
                     vacant_smo_seats += 1
 
-        tot_smo_seats = self.row_smoking_seats
-        print(f'\033There are {tot_smo_seats} smoking seats.')
-        print(f'\033Of these, {vacant_smo_seats} are vacant.')
-        
+        tot_smo_seats = (self.row_smoking_seats * 6)
+        print(f'\033[32mThere are {tot_smo_seats} smoking seats, of these, {vacant_smo_seats} are vacant.\033[m')
+
+    def seat_type(self):
+        st = self.seat_input('> Enter seat number (Ex: A2): ')
+        row = int(st[1:])
+        smo_rows = []
+        for c in range(1, self.row_smoking_seats+1):
+            smo_rows.append(16 - c)
+
+        if row in smo_rows:
+            print(f'\033[32m{st} is a smoking seat.')
+        else:
+            print(f'\033[32m{st} is a non-smoking seat.')
