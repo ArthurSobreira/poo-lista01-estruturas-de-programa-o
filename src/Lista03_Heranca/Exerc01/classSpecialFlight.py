19d852612bc94f9c830fea4810d58f46a363b682
Arthur Sobreira <tutusobreira@gmail.com>
2022-06-28 10:13:44 -0300

    Define constructor method

--- src/Lista03_Heranca/Exerc01/classSpecialFlight.py
+++ src/Lista03_Heranca/Exerc01/classSpecialFlight.py
@@ -2,4 +2,10 @@ from src.Lista02_Classes.Exerc03.classFlight import Flight
 
 
 class SpecialFlight(Flight):
-    pass
+    def __init__(self, number, date, smoking_seat):
+        super().__init__(number, date)
+
+        # Define Smoking Seats
+        for col in self.seats_list:
+            for seat in range(1, smoking_seat+1):
+                self.seats_list[col].__setitem__(-seat, 's')
