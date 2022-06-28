38a3a1c777bb81001fa6ba4c0fed657a77c25b9e
Arthur Sobreira <tutusobreira@gmail.com>
2022-06-28 10:58:42 -0300

    Update classSpecialFlight.py file

--- src/Lista03_Heranca/Exerc01/classSpecialFlight.py
+++ src/Lista03_Heranca/Exerc01/classSpecialFlight.py
@@ -3,23 +3,41 @@ from string import ascii_uppercase
 
 
 class SpecialFlight(Flight):
-    def __init__(self, number, date, smoking_seat):
+    def __init__(self, number, date, row_smoking_seats):
         super().__init__(number, date)
 
         # Define Smoking Seats
         for col in ascii_uppercase[:6]:
-            for seat in range(1, smoking_seat+1):
-                self.seats_list[col].__setitem__(-seat, 's')
+            for seat in range(1, row_smoking_seats+1):
+                self.seats_list[col][-seat] = 's'
+        self.__row_smoking_seats = (row_smoking_seats * 6)
+
+    # Getter and Setter __row_smoking_seats
+    @property
+    def row_smoking_seats(self):
+        return self.__row_smoking_seats
+
+    @row_smoking_seats.setter
+    def row_smoking_seats(self, new_row_sts):
+        self.__row_smoking_seats = new_row_sts
 
     def number_vacancies(self):
-        smo_seats, norm_seats = 0, 0
+        vacant_seats = 0
         for col in ascii_uppercase[:6]:
             for seat in self.seats_list[col]:
-                if seat == '.':
-                    norm_seats += 1
-                if seat == 's':
-                    smo_seats += 1
+                if (seat == '.') or (seat == 's'):
+                    vacant_seats += 1
 
-        total_seats = smo_seats + norm_seats
-        print(f'\033[32mThere are {total_seats} seats available.\033[m')
-        print(f'\033[32m{norm_seats} normal seat(s) and {smo_seats} smoking seat(s).\033[m')
+        print(f'\033[32mThere are {vacant_seats} seats available.\033[m')
+
+    def number_smoking_seats(self):
+        vacant_smo_seats = 0
+        for col in ascii_uppercase[:6]:
+            for seat in self.seats_list[col]:
+                if seat == 's':
+                    vacant_smo_seats += 1
+
+        tot_smo_seats = self.row_smoking_seats
+        print(f'\033There are {tot_smo_seats} smoking seats.')
+        print(f'\033Of these, {vacant_smo_seats} are vacant.')
+        
